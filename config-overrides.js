56acc2abd401f09c56bde47d5d2b412620466fa3
Yohan Tancrez <yohantancrez@gmail.com>
2023-09-05 05:07:53 +0200

    removed everything linked to lingui

--- config-overrides.js
+++ config-overrides.js
@@ -1,18 +1,6 @@
 const webpack = require("webpack");
 
 module.exports = function override(config) {
-  // https://github.com/lingui/js-lingui/issues/1195
-  // Adding loader to use for .po files to webpack
-  const loaders = config.module.rules.find((rule) => Array.isArray(rule.oneOf));
-  loaders.oneOf.splice(loaders.length - 1, 0, {
-    test: /\.po/,
-    use: [
-      {
-        loader: "@lingui/loader",
-      },
-    ],
-  });
-
   config.resolve.fallback = {
     os: false,
     http: false,
